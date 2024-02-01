8a37928d320b6912d85458b91d67f79d0118fdce
Musdate <bastiantroncoso97@gmail.com>
2024-02-01 08:46:17 -0300

    [FIX] Route to prod

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,7 @@
 export const environment = {
 
     production: false,
-    // baseUrl: 'http://localhost:3000'
-    baseUrl: 'https://nest-form-backend.up.railway.app'
+    baseUrl: 'http://localhost:3000'
+    // baseUrl: 'https://nest-form-backend.up.railway.app'
 
 }
